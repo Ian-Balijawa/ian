d1450537d1ff22120e0f6f24fe77b591ddad9f9e
ianbalijawa <ianbalijawa@gmail.com>
2023-01-18 16:51:25 +0300

    complete Major UI

--- src/components/Testimonial/index.tsx
+++ src/components/Testimonial/index.tsx
@@ -1,4 +1,42 @@
+import './testimonial.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import AVATAR from '@assets/me.png';
+import { Pagination } from 'swiper';
+
 const Testimonial = () => {
-    return <div>Testimonial</div>;
+    return (
+        <section id="testimonial">
+            <h5>Review from Clients</h5>
+            <h2>Testimonials</h2>
+            <Swiper
+                className="container testimonials__container"
+                modules={[Pagination]}
+                spaceBetween={50}
+                navigation
+                pagination={{ clickable: true }}
+            >
+                {Array.from({ length: 5 }).map((client, index) => (
+                    <SwiperSlide className="testimonial" key={index}>
+                        <div className="client__avatar">
+                            <img src={AVATAR} alt="client avatar" />
+                        </div>
+                        <h5 className="client__name">Ernest Achiever</h5>
+                        <small className="client__review">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Ab aspernatur suscipit atque rem culpa quas
+                            quae, incidunt aliquam voluptas repudiandae magnam
+                            expedita eum recusandae inventore commodi similique
+                            sed laudantium asperiores delectus? Quibusdam optio
+                            sint blanditiis iste reiciendis! Tempora, sit non.
+                        </small>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
+    );
 };
 export default Testimonial;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import './index.css';
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from '@/components/Footer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
+            <Footer />
         </BrowserRouter>
     </React.StrictMode>
 );
