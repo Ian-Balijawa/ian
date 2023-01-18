c20aa821e10b87274625e805a69d322003bad3d2
ianbalijawa <ianbalijawa@gmail.com>
2023-01-18 20:48:59 +0300

    install node-types

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
-import path from 'path';
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
