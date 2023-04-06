3847960fd52f12c757902eb4100baab1edb69cb8
ian balijawa <ianbalijawa16@gmail.com>
2023-04-06 02:43:11 -0700

    setup server config in vite-conf

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 3000,
+        open: true,
+        cors: process.env.NODE_ENV === 'development',
+    },
     resolve: {
         alias: [
             { find: '@', replacement: path.resolve(__dirname, 'src') },
